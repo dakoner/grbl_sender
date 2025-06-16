e7720fed9ac3e389f33bf317f60eb71f614a257f
David Konerding <dakoner@gmail.comwq>
2025-06-15 18:16:07 -0700

    Initial checkin of grbl_sender

---
+++ grbl_sender.py
@@ -0,0 +1,18 @@
+import sys
+#sys.path.append("..")
+#import mvsdk
+import signal
+from PyQt6 import QtWidgets
+from main_window import MainWindow
+
+
+class QApplication(QtWidgets.QApplication):
+    def __init__(self, *argv):
+        super().__init__(*argv)
+        self.main_window = MainWindow()
+        self.main_window.show()#showMaximized()
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    app = QApplication(sys.argv)    
+    app.exec()
